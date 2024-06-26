c05c0475235adb1acfca09c2050a733b066f14fd
shabashaikh786 <shaba.xworkz@gmail.com>
2024-06-26 14:37:25 +0530

    task database

---
+++ project.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE project;
+CREATE TABLE Airport_details(id int , airport_name varchar(20), price int, arrival time, departure time);
+
+CREATE TABLE Cricket_teams(id int, team_name varchar(30), no_of_players int , captain varchar(30), coach varchar(20));
