9c8f82713146dfb2e09f19543effcddc5ae6e567
shabashaikh786 <shaba.xworkz@gmail.com>
2024-07-03 20:06:41 +0530

    Task

--- TASK.sql
+++ TASK.sql
@@ -26,3 +26,98 @@ insert into product values(7,'chacolate',20,'omesh','2024-04-09','dark',100);
 insert into product values(8,'maggie',20,'omesh','2023-04-09','noodles',25);
 insert into product values(9,'sweets',20,'omesh','2026-04-09','balaji',20);
 insert into product values(10,'light',100,'james','2025-04-09','suraj',15);
+
+USE TASK;
+select *from employee;
+SELECT * FROM EMPLOYEE  WHERE ID=1 AND EMP_NAME = 'suresh'; 
+SELECT * FROM EMPLOYEE  WHERE mgr_id=2001 AND EMP_Mng_NAME = 'mahesh'; 
+SELECT * FROM EMPLOYEE  WHERE ID=2 AND EMP_NAME = 'rupa'; 
+SELECT * FROM EMPLOYEE  WHERE mgr_id=2004 AND EMP_NAME = 'ravi'; 
+SELECT * FROM EMPLOYEE  WHERE ID=1 AND EMP_EXP = 4; 
+
+select *from product;
+SELECT * FROM product  WHERE ID=1 AND mfg_by = 'omesh'; 
+SELECT * FROM product  WHERE ID=2 AND mfg_by = 'omesh'; 
+SELECT * FROM product  WHERE ID=3 AND mfg_by = 'omesh'; 
+SELECT * FROM product  WHERE ID=4 AND mfg_by = 'omesh'; 
+SELECT * FROM product  WHERE ID=5 AND mfg_by = 'omesh'; 
+
+SELECT * FROM EMPLOYEE  WHERE ID=1 OR EMP_NAME = 'suresh'; 
+SELECT * FROM EMPLOYEE  WHERE mgr_id=2001 OR EMP_Mng_NAME = 'mahesh'; 
+SELECT * FROM EMPLOYEE  WHERE ID=2 OR EMP_NAME = 'rupa'; 
+SELECT * FROM EMPLOYEE  WHERE mgr_id=2004 OR EMP_NAME = 'ravi'; 
+SELECT * FROM EMPLOYEE  WHERE ID=1 OR EMP_EXP = 4; 
+
+SELECT * FROM product  WHERE ID=1 OR mfg_by = 'omesh'; 
+SELECT * FROM product  WHERE ID=2 OR mfg_by = 'omesh'; 
+SELECT * FROM product  WHERE ID=3 OR mfg_by = 'omesh'; 
+SELECT * FROM product  WHERE ID=4 OR mfg_by = 'omesh'; 
+SELECT * FROM product  WHERE ID=5 OR mfg_by = 'omesh'; 
+
+
+SELECT * FROM EMPLOYEE  WHERE ID IN (1,2,3); 
+SELECT * FROM EMPLOYEE  WHERE mgr_id IN (2001,2002,2003); 
+SELECT * FROM EMPLOYEE  WHERE ID IN (4,5,6); 
+SELECT * FROM EMPLOYEE  WHERE mgr_id IN (2004,2005); 
+SELECT * FROM EMPLOYEE  WHERE ID IN (7,8,9); 
+
+
+
+SELECT * FROM product  WHERE id in (1,2,3);
+SELECT * FROM product  WHERE prd_price in (20,30);
+SELECT * FROM product  WHERE id in (4,5,6);
+SELECT * FROM product  WHERE prd_quantity in (10,20);
+SELECT * FROM product  WHERE id in (6,7,8);
+
+SELECT * FROM EMPLOYEE  WHERE ID NOT IN (1,2,3); 
+SELECT * FROM EMPLOYEE  WHERE mgr_id NOT IN (2001,2002,2003); 
+SELECT * FROM EMPLOYEE  WHERE ID  NOT IN (4,5,6); 
+SELECT * FROM EMPLOYEE  WHERE mgr_id NOT IN (2004,2005); 
+SELECT * FROM EMPLOYEE  WHERE ID NOT IN (7,8,9); 
+
+SELECT * FROM product  WHERE id NOT IN (1,2,3);
+SELECT * FROM product  WHERE prd_price NOT IN (20,30);
+SELECT * FROM product  WHERE id NOT IN (4,5,6);
+SELECT * FROM product  WHERE prd_quantity NOT IN (10,20);
+SELECT * FROM product  WHERE id NOT IN (6,7,8);
+
+SELECT * FROM EMPLOYEE  WHERE ID BETWEEN 1 AND 5; 
+SELECT * FROM EMPLOYEE  WHERE ID BETWEEN 2 AND 9; 
+SELECT * FROM EMPLOYEE  WHERE ID BETWEEN 4 AND 10; 
+SELECT * FROM EMPLOYEE  WHERE ID BETWEEN 4 AND 7; 
+SELECT * FROM EMPLOYEE  WHERE ID BETWEEN 5 AND 10; 
+
+
+SELECT * FROM product  WHERE id BETWEEN 1 AND 5;
+SELECT * FROM product  WHERE id BETWEEN 1 AND 10;
+SELECT * FROM product  WHERE id BETWEEN 2 AND 5 ;
+SELECT * FROM product  WHERE id BETWEEN 3 AND 5;
+SELECT * FROM product  WHERE id BETWEEN 4 AND 9;
+
+SELECT * FROM EMPLOYEE  WHERE ID NOT BETWEEN 1 AND 5; 
+SELECT * FROM EMPLOYEE  WHERE ID NOT BETWEEN 2 AND 9; 
+SELECT * FROM EMPLOYEE  WHERE ID NOT BETWEEN 4 AND 10; 
+SELECT * FROM EMPLOYEE  WHERE ID NOT BETWEEN 4 AND 7; 
+SELECT * FROM EMPLOYEE  WHERE ID NOT BETWEEN 5 AND 10; 
+
+
+SELECT * FROM product  WHERE id NOT BETWEEN 1 AND 5;
+SELECT * FROM product  WHERE id NOT BETWEEN 1 AND 10;
+SELECT * FROM product  WHERE id NOT BETWEEN 2 AND 5 ;
+SELECT * FROM product  WHERE id NOT BETWEEN 3 AND 5;
+SELECT * FROM product  WHERE id NOT BETWEEN 4 AND 9;
+
+SELECT * FROM employee  WHERE emp_name like 's%';
+SELECT * FROM employee  WHERE emp_name like 'r%';
+SELECT * FROM employee  WHERE emp_name like '%a';
+SELECT * FROM employee  WHERE emp_name like 's%m%a';
+SELECT * FROM employee  WHERE emp_name like '%h';
+
+SELECT * FROM product  WHERE mfg_by like 'o%';
+SELECT * FROM product  WHERE mfg_by like 'o%s%h';
+SELECT * FROM product  WHERE mfg_by like '%a';
+SELECT * FROM product  WHERE mfg_by like 's%m%a';
+SELECT * FROM product  WHERE mfg_by like '%h';
+
+
+
